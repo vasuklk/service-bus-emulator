a7a946e48611d304c2a91e03afaf3e9d1071d272
vasuklk97 <58772150+vasuklk97@users.noreply.github.com>
2026-02-01 13:27:41 +0530

    Update TopicTests.cs

--- Publisher.Tests/TopicTests.cs
+++ Publisher.Tests/TopicTests.cs
@@ -14,47 +14,6 @@ namespace Publisher.Tests
         ServiceBusClient client = new ServiceBusClient(connectionString);
 
         [Fact]
-        public async Task SendAndReceiveMessage_ToTopic()
-        {
-            
-            string body = $"test-message-{Guid.NewGuid()}";
-
-            await using var client = new ServiceBusClient(connectionString);
-            ServiceBusSender sender = client.CreateSender(topicName);
-            var messageId = Guid.NewGuid().ToString();
-            var message1 = new ServiceBusMessage(body) { MessageId = messageId };
-            message1.ApplicationProperties["UserType"] = "Admin";
-            message1.ApplicationProperties["UserRole"] = "Guest";
-            var message2 = new ServiceBusMessage(body) { MessageId = messageId };
-            message2.ApplicationProperties["UserType"] = "User";
-            await sender.SendMessagesAsync(messages: new[] { message1, message2 });
-
-            ServiceBusReceiver receiver = client.CreateReceiver(topicName, subscriptionName, new ServiceBusReceiverOptions
-            {
-                ReceiveMode = ServiceBusReceiveMode.PeekLock
-            });
-
-            ServiceBusReceivedMessage received = null;
-            var responses = await receiver.ReceiveMessagesAsync(3, TimeSpan.FromSeconds(5));
-            foreach (var r in responses)
-            {
-                if (r.MessageId == messageId)
-                {
-                    received = r;
-                    await receiver.CompleteMessageAsync(r);
-                    break;
-                }
-                else
-                {
-                    await receiver.AbandonMessageAsync(r);
-                }
-            }
-
-            Assert.NotNull(received);
-            Assert.Equal(body, received.Body.ToString());
-        }
-
-        [Fact]
         public async Task SendAndMessage_ToTopic()
         {
             
